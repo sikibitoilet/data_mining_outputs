607dcb8c3e8b105b5d6f19a7016a1d6ae85998d5
unknown <https://github.com/Dianetmasunget/IM2_project>
2025-09-15 22:00:39 +0800

    v1

---
+++ Assignment 2.R
@@ -0,0 +1,12 @@
+grade <- 72
+if (grade >=90 && grade<=100){
+  print("Grade: C")
+} else if (grade >= 80 && grade < 90) {
+  print("Grade: B")
+} else if (grade >= 70 && grade < 80) {
+  print("Grade: C")
+} else if (grade >= 60 && grade < 70) {
+  print("Grade: D")
+} else if (grade < 60){
+  print("Grade: F")
+}
